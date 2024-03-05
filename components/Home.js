1578229490b0f338ba7f38be7f2f4026c45ea780
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-03-04 20:46:48 -0600

    adds files for class 39/41

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import { useAuth } from '../contexts/auth';
-import CookieStandAdmin from '../components/CookieStandAdmin';
-import LoginForm from '../components/LoginForm';
+import CookieStandAdmin from './CookieStandAdmin';
+import LoginForm from './LoginForm';
 
 export default function Home() {
 
     const { user, login } = useAuth();
-    
+
     return (
         <div className="p-4">
             <Head>
